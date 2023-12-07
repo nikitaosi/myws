0832eafe8b5922f2364289dcd7a1f2f6312d48ae
Nikita Osipov <nikitaosipov.51@gmail.com>
2023-12-07 19:25:03 +0400

    Remove theme-util.js and refactor theme handling
    The commit includes the removal of theme-util.js and the transition of theme handling to a CSS class-based approach, leveraging Tailwind CSS's darkMode feature. It also involves modifying theme switcher in ThemeSlider component and adding a script in the head of the BaseLayout to load the preferred theme from localStorage on initial load. Removal of several unnecessary old theme handling CSS rules from global.css file is also part of the update.

---
+++ src/lib/theme-util.ts
@@ -0,0 +1,23 @@
+  const select = document.getElementById("themeSwitch") as HTMLSelectElement;
+  const theme = localStorage.getItem("theme");
+  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
+
+  console.log('theme');
+
+  select.value = theme ?? systemTheme;
+
+  function updateTheme(value) {
+  const theme = value === "system" ? systemTheme : value;
+  document.documentElement.classList.remove("light", "dark");
+  document.documentElement.classList.add(theme);
+  localStorage.setItem("theme", value);
+}
+
+  updateTheme(select.value);
+
+  select.addEventListener("change", (event: Event) => {
+  const select = event.target as HTMLSelectElement;
+  updateTheme(select.value);
+});
